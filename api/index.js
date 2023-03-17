7c4462afd59312e825a42ef5a5d683e4bb836f81
ShiroTanya <phamhoanglong.lk1@gmail.com>
2023-03-17 17:04:42 +0700

    add api auth

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// const authRoute = require("./routes/auth");
+const authRoute = require("./routes/auth");
 // const userRoute = require("./routes/users");
 // const movieRoute = require("./routes/movies");
 // const listRoute = require("./routes/lists");
@@ -13,16 +13,15 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
   })
   .then(() => console.log("DB Connection Successfull"))
   .catch((err) => {
     console.error(err);
   });
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoute);
 // app.use("/api/users", userRoute);
 // app.use("/api/movies", movieRoute);
 // app.use("/api/lists", listRoute);
