2d9a6ecf55d244d47150bfa03175a4a8cd1fcd41
ShiroTanya <phamhoanglong.lk1@gmail.com>
2023-03-17 15:05:23 +0700

    create BE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import "./app.scss"
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 
 const App = () => {
-  return <Home/>;
+  return <Login/>;
+  // return <Register/>;
 };
 
 export default App;
