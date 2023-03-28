64e32c2a7596c989875c85e5c92f7fe074061115
duydat11 <duydat18042k1@gmail.com>
2023-03-28 09:31:05 +0700

    Revert "add react router dom"
    This reverts commit 27d8c4e992d86054234ec64faf7f851debd966ee.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useContext } from 'react';
-// import { AuthContext } from './authContext/AuthContext';
-import Home from './pages/home/Home';
-import Register from './pages/register/Register';
-import Watch from './pages/watch/Watch';
-import Login from './pages/login/Login';
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 
 const App = () => {
-  // const { user } = useContext(AuthContext);
-const user = true;
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={user ? <Home /> : <Navigate to="/register" />} />
-        <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
-        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-        {user && (
-          <>
-            <Route path="/movies" element={<Home type="movie" />} />
-            <Route path="/series" element={<Home type="series" />} />
-            <Route path="/watch" element={<Watch />} />
-          </>
-        )}
-      </Routes>
-    </Router>
-  );
+  return <Login/>;
+  // return <Register/>;
 };
 
 export default App;
